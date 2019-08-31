877de975cdc398e1a88696c583df688afb9667e5
mx6yt <53902424+mx6yt@users.noreply.github.com>
2019-08-31 08:10:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -60,7 +60,7 @@ client.on("message", message => {
     }
     voiceChannel.join()
       .then(connnection => {
-        let stream = yt('https://www.youtube.com/watch?v=HfyJ1ibiaew', {audioonly: true});
+        let stream = yt('youtube.com/watch?v=HfyJ1ibiaew', {audioonly: true});
         const dispatcher = connnection.playStream(stream);
       });
   }
